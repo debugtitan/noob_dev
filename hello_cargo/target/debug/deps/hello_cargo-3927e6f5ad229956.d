2e849e982f231937e3ae525f11b6d5cc0407d631
debugtitan <alts.devs@gmail.com>
2024-05-27 20:49:49 +0100

    cargo tutorial

---
+++ hello_cargo/target/debug/deps/hello_cargo-3927e6f5ad229956.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/UCHE/Desktop/tutorial/hello_cargo/target/debug/deps/hello_cargo-3927e6f5ad229956: src/main.rs
+
+/mnt/c/Users/UCHE/Desktop/tutorial/hello_cargo/target/debug/deps/hello_cargo-3927e6f5ad229956.d: src/main.rs
+
+src/main.rs:
