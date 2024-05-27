4e94f7d8dd93ef9a2a880ae7f78e11816a984034
debugtitan <alts.devs@gmail.com>
2024-05-27 20:56:35 +0100

    cargo done

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,9 @@
+//cargo new : create a project
+// cargo check to check  if program can compile
+//cargo build :  build program executables
+// cargo run : build and run a project in single step
+// cargo build --release : command will create an executable in target/release instead of target/debug.
+
 fn main() {
     println!("Hello, world!");
 }
